7db07624c6abbc519ee65941f04fcbdf2c85b6b9
jkhasizada <javid.khasizada@gmail.com>
2025-01-07 22:28:35 +0400

    Refactor Entries component to improve readability and simplify props handling

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,22 +1,24 @@
 import Entry from "./Entry";
 
-export default function Entries({ entries }) {
+export default function Entries(props) {
+  const entryElements = props.entries.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        image={entry.image}
+        country={entry.country}
+        location={entry.location}
+        dates={entry.dates}
+        description={entry.description}
+        mapsUrl={entry.mapsUrl}
+        score={entry.score}
+      />
+    );
+  });
+
   return (
     <section className="grid grid-cols-1 max-w-6xl w-full mx-auto gap-16 p-8 sm:p-16">
-      {entries.map((entry) => {
-        return (
-          <Entry
-            key={entry.id}
-            image={entry.image}
-            country={entry.country}
-            location={entry.location}
-            dates={entry.dates}
-            description={entry.description}
-            mapsUrl={entry.mapsUrl}
-            score={entry.score}
-          />
-        );
-      })}
+      {entryElements}
     </section>
   );
 }
